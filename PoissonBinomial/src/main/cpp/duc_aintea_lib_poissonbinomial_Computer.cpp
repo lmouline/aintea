318e522bb3ba8a032ffa2b6fec475b97b6c709f4
Ludovic Mouline <ludovic.mouline@uni.lu>
2020-01-13 16:02:46 +0100

    add JNI lib for PoissonBinomial

---
+++ PoissonBinomial/src/main/cpp/duc_aintea_lib_poissonbinomial_Computer.cpp
@@ -0,0 +1,81 @@
+#include <jni.h>
+#include <fftw3.h>
+#include <complex>
+#include "duc_aintea_lib_poissonbinomial_Computer.h"
+
+using namespace std;
+
+void dft_pmf(fftw_complex* out, int m, jdouble* pp);
+
+JNIEXPORT jdoubleArray JNICALL Java_duc_aintea_lib_poissonbinomial_Computer_compute(JNIEnv *env, jobject, jdoubleArray probs) {
+    // Convert jdoubleArray into jdouble*
+    if(probs == NULL) return NULL;
+    jsize size_array_res = env->GetArrayLength(probs) + 1;
+    if(size_array_res == 1) return NULL; // empty input
+    jdouble* array_probs = env->GetDoubleArrayElements(probs, NULL);
+    if(array_probs == NULL) return NULL;
+
+
+    // Compute PoissonBinomial
+    fftw_complex* out;
+    out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * size_array_res);
+    dft_pmf(out, size_array_res, array_probs);
+
+    jdouble res[size_array_res];
+    for(int k = 0; k < size_array_res; ++k)
+    {
+        res[k] = out[k][0] / size_array_res;
+    }
+    fftw_free(out);
+
+    // Convert jdouble* into jdoubleArray
+    jdoubleArray outJNI = env->NewDoubleArray(size_array_res);
+    if(outJNI == NULL) return NULL;
+    env->SetDoubleArrayRegion(outJNI, 0 , size_array_res, res);
+    return outJNI;
+}
+
+void dft_pmf(fftw_complex* out, int m, jdouble* pp) {
+    int n = m - 1;
+    fftw_complex* in;
+    fftw_plan p;
+    in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * m);
+    complex<double> C(0.0,2);
+    C = exp(C * 3.1415926535897 / ((double)m));
+    double C_real = C.real();
+    double C_imag = C.imag();
+
+
+    //build input vector
+    double tmp_real;
+    double tmp_imag;
+    std::complex<double> temp;
+    std::complex<double> f(1.,0.0);
+    in[0][0] = 1.0;
+    in[0][1] = 0.0;
+
+
+    int halfn = n / 2 + 1;
+    for (int i = 1; i <= halfn; ++i){
+        temp = 1.;
+        tmp_real = f.real();
+        tmp_imag = f.imag();
+        f.real(tmp_real * C_real - tmp_imag * C_imag);
+        f.imag(tmp_imag * C_real + tmp_real * C_imag);
+        for(int j = 0; j < n; ++j){
+            temp *= (1. + (f - 1.) * pp[j]);
+        }
+
+        in[i][0] = temp.real();
+        in[i][1] = temp.imag();
+        in[m-i][0] = temp.real();
+        in[m-i][1] = - temp.imag();
+    }
+
+
+    //dft
+    p = fftw_plan_dft_1d(m, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+    fftw_execute(p);
+    fftw_destroy_plan(p);
+    fftw_free(in);
+}
---
+++ PoissonBinomial/src/main/java/duc/aintea/lib/poissonbinomial/Computer.java
@@ -0,0 +1,44 @@
+package duc.aintea.lib.poissonbinomial;
+
+import java.io.*;
+
+public class Computer {
+
+    static {
+        String osName = System.getProperty("os.name");
+        String target;
+        if(osName.equals("Mac OS X")) {
+            target = "lib/libpoissonbinomial.dylib";
+        } else {
+            throw new RuntimeException("No library version for " + osName + ". Sorry :)");
+        }
+
+        try {
+            File extractedFile = File.createTempFile("aintea_", ".dylib", null);
+            extractedFile.deleteOnExit();
+            InputStream isLib = Computer.class.getClassLoader().getResourceAsStream(target);
+            if(isLib == null) {
+                isLib = ClassLoader.getSystemResourceAsStream(target);
+            }
+            if(isLib == null) {
+                throw new RuntimeException("Library not found at " + target);
+            }
+
+            OutputStream outLib = new FileOutputStream(extractedFile);
+            byte[] buf = new byte[1024];
+            int len;
+            while((len = isLib.read(buf)) > 0) {
+                outLib.write(buf, 0, len);
+            }
+            outLib.close();
+            isLib.close();
+
+            System.load(extractedFile.getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public native double[] compute(double[] probs);
+
+}
